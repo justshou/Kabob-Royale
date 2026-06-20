4c77a443a06a4caa1cc04c3f99726e52301393f6
Tony Shou <a.mashou04@gmail.com>
2026-06-20 14:58:43 -0400

    Refactor Navbar component to use a nav element and improve layout with responsive button classes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 export default function Navbar() {
+  const buttonClasses =
+    "px-3 py-2 text-sm font-medium hover:text-yellow-500 transition-colors";
+  const buttons = (
+    <>
+      <button className={buttonClasses}>Home</button>
+      <button className={buttonClasses}>Menu</button>
+    </>
+  );
+
   return (
-    <header className="p-4 bg-white shadow sticky top-0 z-10">
-      <div className="max-w-4xl mx-auto font-semibold">Kabob Royale</div>
-    </header>
+    <nav className="bg-stone-900 text-yellow-600 fixed w-full">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="text-xl font-bold">Kabob Royale</div>
+          {/* Need to redo this to work on mobile */}
+          <div className="hidden md:block">
+            <div className="flex ml-10 items-baseline space-x-2">{buttons}</div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
